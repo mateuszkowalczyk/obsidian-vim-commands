a8a008b3a850e1bf7faf20a10464c8f421f91a3f
Mateusz Kowalczyk <mateuszmkowalczyk@gmail.com>
2026-07-09 18:09:02 +0200

    Implement new Obsidian settings format

--- src/settings.ts
+++ src/settings.ts
@@ -10,6 +10,9 @@ export const DEFAULT_SETTINGS: VimCommandsPluginSettings = {
 	configFilePath: DEFAULT_CONFIG_FILE_PATH,
 };
 
+const CONFIG_FILE_PATH_NAME = 'Config file path';
+const CONFIG_FILE_PATH_DESC = 'Path to the vimrc-style command mapping file in this vault.';
+
 export class VimCommandsSettingTab extends PluginSettingTab {
 	plugin: VimCommandsPlugin;
 
@@ -18,23 +21,37 @@ export class VimCommandsSettingTab extends PluginSettingTab {
 		this.plugin = plugin;
 	}
 
+	getSettingDefinitions(): unknown[] {
+		return [
+			{
+				name: CONFIG_FILE_PATH_NAME,
+				desc: CONFIG_FILE_PATH_DESC,
+				render: (setting: Setting) => this.addConfigFilePathControl(setting),
+			},
+		];
+	}
+
 	display(): void {
 		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Config file path')
-			.setDesc('Path to the vimrc-style command mapping file in this vault.')
-			.addText((text) =>
-				text
-					.setPlaceholder(DEFAULT_CONFIG_FILE_PATH)
-					.setValue(this.plugin.settings.configFilePath)
-					.onChange(async (value) => {
-						this.plugin.settings.configFilePath = value;
-						await this.plugin.saveSettings();
-						await this.plugin.reloadMappings();
-					}),
-			);
+			.setName(CONFIG_FILE_PATH_NAME)
+			.setDesc(CONFIG_FILE_PATH_DESC)
+			.then((setting) => this.addConfigFilePathControl(setting));
+	}
+
+	private addConfigFilePathControl(setting: Setting): void {
+		setting.addText((text) =>
+			text
+				.setPlaceholder(DEFAULT_CONFIG_FILE_PATH)
+				.setValue(this.plugin.settings.configFilePath)
+				.onChange(async (value) => {
+					this.plugin.settings.configFilePath = value;
+					await this.plugin.saveSettings();
+					await this.plugin.reloadMappings();
+				}),
+		);
 	}
 }
